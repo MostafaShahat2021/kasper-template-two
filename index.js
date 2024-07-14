a9fb0fcc1e2e37c965c535665019b298269329d3
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2024-07-14 12:09:37 +0300

    Enhance plan hover effect

--- index.js
+++ index.js
@@ -6,15 +6,22 @@ const mainColor = "#19c8fa"
 plans.forEach(plan => {
   const foot = plan.querySelector('.foot');
   const headSpan = plan.querySelector('.head span');
+  const headSub = plan.querySelector('.head sub');
+  const headSup = plan.querySelector('.head sup');
 
 
   foot.addEventListener('mouseover', () => {
     headSpan.style.color = mainColor;
+    headSub.style.color = mainColor
+    headSup.style.color = mainColor
     plan.style.border = `1px solid ${mainColor}` 
   });
 
+  // Reset color on mouseout
   foot.addEventListener('mouseout', () => {
-    headSpan.style.color = ''; // Reset color on mouseout
-    plan.style.border = "none" 
+    headSpan.style.color = '';
+    headSub.style.color = "";
+    headSup.style.color = "";
+    plan.style.border = "none"; 
   });
 });
