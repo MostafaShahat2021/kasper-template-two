f5df38733e2fd046d161d26268007577be5c8d4c
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2024-07-16 11:53:29 +0300

    format decoment

--- index.js
+++ index.js
@@ -1,46 +1,43 @@
-
 // Toggle menu
-const toggleMenu = document.querySelector(".toggle-menu")
-const navMenu = document.querySelector('.nav-menu')
+const toggleMenu = document.querySelector('.toggle-menu');
+const navMenu = document.querySelector('.nav-menu');
 
-toggleMenu.addEventListener("click", () =>{
-  navMenu.classList.toggle("show")
-})
+toggleMenu.addEventListener('click', () => {
+  navMenu.classList.toggle('show');
+});
 
 // Toggle active Class on Nav Item Click with (Event Delegation)
-navMenu.addEventListener("click", (e)=>{
-  const target = e.target
-  if(target.classList.contains("nav-link")){
-    const navLinks = document.querySelectorAll('.nav-link')
-    navLinks.forEach((link) => link.classList.remove("active"))
-    target.classList.add("active")
+navMenu.addEventListener('click', (e) => {
+  const target = e.target;
+  if (target.classList.contains('nav-link')) {
+    const navLinks = document.querySelectorAll('.nav-link');
+    navLinks.forEach((link) => link.classList.remove('active'));
+    target.classList.add('active');
   }
-})
-
+});
 
 // plan hover effect
 const plans = document.querySelectorAll('.plan');
-const mainColor = "#19c8fa"
+const mainColor = '#19c8fa';
 
-plans.forEach(plan => {
+plans.forEach((plan) => {
   const foot = plan.querySelector('.foot');
   const headSpan = plan.querySelector('.head span');
   const headSub = plan.querySelector('.head sub');
   const headSup = plan.querySelector('.head sup');
 
-
   foot.addEventListener('mouseover', () => {
     headSpan.style.color = mainColor;
-    headSub.style.color = mainColor
-    headSup.style.color = mainColor
-    plan.style.border = `1px solid ${mainColor}` 
+    headSub.style.color = mainColor;
+    headSup.style.color = mainColor;
+    plan.style.border = `1px solid ${mainColor}`;
   });
 
   // Reset color on mouseout
   foot.addEventListener('mouseout', () => {
     headSpan.style.color = '';
-    headSub.style.color = "";
-    headSup.style.color = "";
-    plan.style.border = "none"; 
+    headSub.style.color = '';
+    headSup.style.color = '';
+    plan.style.border = 'none';
   });
 });
